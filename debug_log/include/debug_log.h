957788140e8a106a03c7564b6eb8767a8bf22cd0
orionxer <orionxer@gmail.com>
2024-02-13 11:35:10 +0800

    ✨增加终端彩色打印工程

---
+++ debug_log/include/debug_log.h
@@ -0,0 +1,121 @@
+﻿#ifndef DEBUG_LOG_H_
+#define DEBUG_LOG_H_
+#include <stdint.h>
+#include <string.h>
+
+/**
+ * @brief   尽可能确保显示的终端支持ANSI的转移序列以及UTF-8的显示
+ * @note    当终端不支持ANSI的转移序列，则无法正常显示颜色信息
+ * @note    当终端不支持UTF-8，则关于中文的显示会乱码
+*/
+
+/************************** 用户自定义宏 *********************************/
+/**
+ * @brief   调试输出总开关
+ * @note    处于禁用状态时关闭所有输出
+*/
+#define DGB_ENABLE          1
+
+/**
+ * @brief   指定 输出打印等级
+ * @note    1. 只会打印当前等级及其以上的等级的信息
+ * @note    2. 假设 指定的打印等级 = DGB_LOG_WARNING,
+ *          则只会打印 DGB_LOG_WARNING 和 DGB_LOG_ERROR两个等级的信息
+*/
+#define DBG_LOG_LEVEL       DGB_LOG_DEBUG
+
+
+/*************************** 调试输出保留宏 ********************************/
+/**
+ * @brief   输出的等级从高到低的排序: [错误] > [警告] > [普通] > [调试]
+*/
+// [错误]输出等级 
+#define DGB_LOG_ERROR       1
+// [警告]输出等级
+#define DGB_LOG_WARNING     2
+// [普通]输出等级
+#define DGB_LOG_INFO        3
+// [调试]输出等级
+#define DGB_LOG_DEBUG       4
+
+#define ANSI_COLOR_RED      "\x1b[31m"
+#define ANSI_COLOR_GREEN    "\x1b[32m"
+#define ANSI_COLOR_YELLOW   "\x1b[33m"
+#define ANSI_COLOR_BLUE     "\x1b[34m"
+#define ANSI_COLOR_MAGENTA  "\x1b[35m"
+#define ANSI_COLOR_CYAN     "\x1b[36m"
+#define ANSI_COLOR_RESET    "\x1b[0m"
+
+// 只获取文件名
+#define filename(x) strrchr(x,'/')?strrchr(x,'/')+1:x
+
+// 调试输出总开关 处于打开状态
+#if DGB_ENABLE
+// [调试]等级控制
+#if DBG_LOG_LEVEL >= DGB_LOG_DEBUG
+#define DBG_LOGD(...) \
+    printf(ANSI_COLOR_BLUE); \
+    printf("[%s]: ", __func__); \
+    printf(__VA_ARGS__); \
+    printf(ANSI_COLOR_RESET); \
+    printf("\n");
+#else
+#define DBG_LOGD(...)
+#endif
+
+// [普通]等级控制
+#if DBG_LOG_LEVEL >= DGB_LOG_INFO
+#define DBG_LOGI(...) \
+    printf(ANSI_COLOR_GREEN); \
+    printf("[%s]: ", __func__); \
+    printf(__VA_ARGS__); \
+    printf(ANSI_COLOR_RESET); \
+    printf("\n");
+#else
+#define DBG_LOGI(...)
+#endif
+
+// [警告]等级控制
+#if DBG_LOG_LEVEL >= DGB_LOG_WARNING
+#define DBG_LOGW(...) \
+    printf(ANSI_COLOR_YELLOW); \
+    printf("[%s:%d %s]: ", filename(__FILE__), __LINE__, __func__); \
+    printf(__VA_ARGS__); \
+    printf(ANSI_COLOR_RESET); \
+    printf("\n");
+#else
+#define DBG_LOGW(...)
+#endif
+
+// [错误]等级控制
+#if DBG_LOG_LEVEL >= DGB_LOG_ERROR
+#define DBG_LOGE(...) \
+    printf(ANSI_COLOR_RED); \
+    printf("[%s:%d %s]: ", filename(__FILE__), __LINE__, __func__); \
+    printf(__VA_ARGS__); \
+    printf(ANSI_COLOR_RESET); \
+    printf("\n");
+#else
+#define DBG_LOGE(...)
+#endif
+#else
+#define DBG_LOGD(...)
+#define DBG_LOGI(...)
+#define DBG_LOGW(...)
+#define DBG_LOGE(...)
+#endif
+/**
+ * @brief   以16进制打印数据
+ * @param   [in] data   打印的数据数据
+ * @param   [in] len    数据长度 
+ * @note    输出格式 = %02X
+*/
+void print_hex_table(uint8_t *data, uint16_t len);
+
+/**
+ * @brief   彩色打印示例
+ * @note    打印各个等级的信息以及16进制的格式化输出
+*/
+void debug_log_demo(void);
+
+#endif
---
+++ debug_log/source/debug_log.c
@@ -0,0 +1,56 @@
+﻿#include <stdio.h>
+#include "debug_log.h"
+
+
+void print_hex_table(uint8_t *data, uint16_t len)
+{
+#if DGB_ENABLE
+    // 打印表头
+    printf("     ");
+    printf(ANSI_COLOR_MAGENTA);
+    for (size_t i = 0; i < 0x10; i++)
+    {
+        printf("%02X ", (unsigned int)i);
+    }
+    printf(ANSI_COLOR_RESET);
+    printf("\n");
+    // 打印表列以及有效数据, 每打印16个数据换一行
+    for (size_t i = 0; i < len; i++)
+    {
+        if (i % 16 == 0)
+        {
+            printf(ANSI_COLOR_MAGENTA);
+            printf("%04X ", (unsigned int)i);
+            printf(ANSI_COLOR_RESET);
+        }
+        printf(ANSI_COLOR_CYAN);
+        printf("%02X ", data[i]);
+        printf(ANSI_COLOR_RESET);
+        if ((i + 1) % 16 == 0)
+        {
+            printf("\n");
+        }
+    }
+    printf("\n");
+#endif
+}
+
+void debug_log_demo(void)
+{
+    DBG_LOGI("中文测试输出, 需要确保终端支持UTF-8的显示, 否则显示乱码\n");
+    // 输出所有等级测试，可以尝试修改打印等级的宏以观察输出效果 DBG_LOG_LEVEL
+    DBG_LOGD("The Log Level is Debug");
+    DBG_LOGI("The Log Level is Info");
+    DBG_LOGW("The Log Level is Warning");
+    DBG_LOGE("The Log Level is Error\n");
+    // 打印16进制
+    uint8_t array[100] = {0};
+    for (size_t i = 0; i < 100; i++)
+    {
+        array[i] = i;
+    }
+    print_hex_table(array, 100);
+#if DGB_ENABLE == 0
+    printf("## Debug Print Disable, NO log print\n");
+#endif
+}
